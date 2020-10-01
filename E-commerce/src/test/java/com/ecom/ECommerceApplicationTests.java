145bd73802306bc145eeb4a158ede212d4deec39
SAM <SAM@DESKTOP-MGQLQ02>
2020-10-01 12:40:14 +0530

    3rd commit

--- E-commerce/src/test/java/com/ecom/ECommerceApplicationTests.java
+++ E-commerce/src/test/java/com/ecom/ECommerceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cts;
+package com.ecom;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CtsECommerceApplicationTests {
+class ECommerceApplicationTests {
 
 	@Test
 	void contextLoads() {
